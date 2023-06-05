1aba6e921f9d745f370d32faf435781df1946664
Santosh <ssantoshpro@gmail.com>
2023-06-05 22:55:36 +0530

    Hyperparameters code added

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -54,9 +54,55 @@ class ModelTrainer:
                 "AdaBoost Regressor": AdaBoostRegressor(),
                 "Gradiant Boosting":GradientBoostingRegressor(),
             }
+
+            params={
+                "Linear Regression":{},
+                "Lasso": {
+                    'alpha':[0.25, 0.5, 0.75, 1.0 , 1.25 , 1.5, 1.75, 2.0],
+                    },
+                "Ridge": {
+                    'alpha':[0.25, 0.5, 0.75, 1.0 , 1.25 , 1.5, 1.75, 2.0],
+                    },
+                "K-Neighbors Regressor": {'n_neighbors': [2,3,4,5,6],
+                                           'weights': ['uniform','distance']
+                                           },
+                "Decision Tree": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'splitter':['best','random'],
+                    # 'max_features':['sqrt','log2'],
+                        },
+                "Random Forest Regressor":{
+                    # 'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                    # 'max_features':['sqrt','log2',None],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoosting Regressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoost Regressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    # 'loss':['linear','square','exponential'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "Gradiant Boosting":{
+                    # 'loss':['squared_error', 'huber', 'absolute_error', 'quantile'],
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    # 'criterion':['squared_error', 'friedman_mse'],
+                    # 'max_features':['auto','sqrt','log2'],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                
+            }
             
             model_report: dict = evaluate_model(X_train=X_train, y_train=y_train, X_test=X_test, y_test=y_test, 
-                                                models=models)
+                                                models=models, param=params)
             
             logging.info("Evaluated Model -- Proceeding to select best model")
             
@@ -74,6 +120,7 @@ class ModelTrainer:
                 file_path = self.model_trainer_config.trained_model_file_path,
                 obj = best_model
             )
+
             predicted=best_model.predict(X_test)
 
             r2_square = r2_score(y_test, predicted)
--- src/utils.py
+++ src/utils.py
@@ -7,6 +7,7 @@ from src.exception import CustomException
 import dill
 from src.logger import logging
 from sklearn.metrics import mean_squared_error, r2_score
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     logging.info("*** Function called - save_object() ***")
@@ -22,21 +23,26 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-def evaluate_model(X_train, y_train, X_test, y_test, models :dict):
+def evaluate_model(X_train, y_train, X_test, y_test, models, param):
     try:
         report = {}
 
-        for key, model in models.items():
-            model.fit(X_train, y_train)
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]]
 
-            # y_train_pred = model.predict(X_train)
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
             y_test_pred = model.predict(X_test)
-            
-            # train_model_score = r2_score(y_train, y_train_pred)
+
             test_model_score = r2_score(y_test, y_test_pred)
-            # print(test_model_score)   
-            report[key] = test_model_score
-            
+
+            report[list(models.keys())[i]] = test_model_score
+
         return report
 
     except Exception as e:
