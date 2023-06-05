b8a8188be5b7f2741fb20bfd0cc4654c17a72e5c
Santosh <ssantoshpro@gmail.com>
2023-06-05 18:05:38 +0530

    Modified Cat_Transformer

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -47,7 +47,7 @@ class DataTransformation():
                 steps=[
                     ("imputer", SimpleImputer(strategy="most_frequent")),
                     ("one_hot_encoder", OneHotEncoder()),
-                    # ("scaler", StandardScaler())
+                    ("scaler", StandardScaler())
                 ],
             )
 
